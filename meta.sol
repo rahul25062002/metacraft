23f91ee7112459323819adf19c2405a427b0b0bb
rahul25062002 <75932347+rahul25062002@users.noreply.github.com>
2023-10-23 00:28:55 +0530

    Create meta.sol

---
+++ meta.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+    string public TokenName = "Rahul";
+    string public tokenSymbol = "Rk";
+    uint public totalAmount = 0;
+
+    // mapping variable
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint(address _address, uint _value) public {
+        totalAmount += _value;
+        balances[_address] += _value;
+    }
+
+    // burn function
+    function burn(address _address, uint _value) public {
+        if(balances[_address] >= _value) {
+            totalAmount -= _value;
+            balances[_address] -= _value;
+        } 
+    }
+}
